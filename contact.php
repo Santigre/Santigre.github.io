1942d35a510a2f69e57c283bc1123638164f89a9
Santiago Beltran <beltran.santiago1@gmail.com>
2019-01-16 21:31:47 -0800

    added contact.php

---
+++ contact.php
@@ -0,0 +1,16 @@
+﻿<?php
+if(isset($_POST["submit"])) {
+	$name=$_POST["name"];
+	$subject=$_POST["subject"]
+	$emailFrom=$_POST["mail"];
+	$message=$_POST["message"];
+
+    $recipient="beltran.santiago1@gmail.com";
+	$headers ="From: ".$emailFrom;
+	$txt = "You have received an email from ".$name.".\n\n"
+
+	mail($recipient, $subject, $txt, $headers);
+
+	header(Location: index.html?mailsent);
+}
+?>
