aef72852b7efb36f67969c00f88639f47bd7130d
Santiago Beltran <beltran.santiago1@gmail.com>
2019-01-16 21:07:21 -0800

    updated
    little to no change

--- index.php
+++ index.php
@@ -19,7 +19,6 @@ if(isset($_POST["submit"])) {
 <html>
 <head>
     <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Santiago Beltran Eportfolio</title>
     <link rel="stylesheet" href="styleSheets/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
