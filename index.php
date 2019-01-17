f9ddfbb947a7f30d4b5f1fa48e94906ed004f9d4
Santiago Beltran <beltran.santiago1@gmail.com>
2019-01-16 22:27:11 -0800

    fixed a couple syntax

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
 ﻿<?php
 if(isset($_POST["submit"])) {
 	$name=$_POST["name"];
-	$subject=$_POST["subject"]
+	$subject=$_POST["subject"];
 	$emailFrom=$_POST["mail"];
 	$message=$_POST["message"];
 
     $recipient="Oruga.95@hotmail.com";
 	$headers ="From: ".$emailFrom;
-	$txt = "You have received an email from ".$name.".\n\n"
+	$txt = "You have received an email from ".$name.".\n\n".$message;
 
 	mail($recipient, $subject, $txt, $headers);
 
-	header(Location: index.php?mailsent);
+	header("Location: index.php?mailsent");
 }
 ?>
 
