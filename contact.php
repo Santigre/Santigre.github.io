bd013e992ec8cf8066ad7405bb02a81e8b439e1a
Santiago Beltran <beltran.santiago1@gmail.com>
2019-01-17 21:10:07 -0800

    Updates
    Tried adding validations to both the php file and index

--- contact.php
+++ contact.php
@@ -1,5 +1,26 @@
 ﻿<?php
-if($_POST["submit"]) {
+if(!isset($_POST['submit']))
+{
+	//This page should not be accessed directly. Need to submit the form.
+	echo "error; you need to submit the form!";
+}
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+//Validate first
+if(empty($name)||empty($visitor_email)) 
+{
+    echo "Name and email are mandatory!";
+    exit;
+}
+
+if(IsInjected($visitor_email))
+{
+    echo "Bad email value!";
+    exit;
+}
+
 	$name= $_POST["name"];
     $emailFrom= $_POST["email"];
 	$subject= $_POST["subject"];
@@ -10,6 +31,28 @@ if($_POST["submit"]) {
 	$txt = "You have received an email from ".$name.".\n\n".$message;
 
 	mail($recipient, $subject, $txt, $headers);
-	header("Location: Index.html?mailsend");
+	header("Location: index.html?mailsend");
+// Function to validate against any email injection attempts
+function IsInjected($str)
+{
+  $injections = array('(\n+)',
+              '(\r+)',
+              '(\t+)',
+              '(%0A+)',
+              '(%0D+)',
+              '(%08+)',
+              '(%09+)'
+              );
+  $inject = join('|', $injections);
+  $inject = "/$inject/i";
+  if(preg_match($inject,$str))
+    {
+    return true;
+  }
+  else
+    {
+    return false;
+  }
 }
-?>
+   
+?> 
