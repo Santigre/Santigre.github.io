23b148d0203ea23227671939e6d79ea9c90eccd4
Santiago Beltran <beltran.santiago1@gmail.com>
2019-01-17 20:32:03 -0800

    trying new ideas

---
+++ scripts/contact.php
@@ -0,0 +1,15 @@
+﻿<?php
+if($_POST["submit"]) {
+	$name= $_POST["name"];
+    $emailFrom= $_POST["email"];
+	$subject= $_POST["subject"];
+	$message= $_POST["message"];
+
+    $recipient= "Oruga.95@hotmail.com";
+	$headers ="From: "$emailFrom;
+	$txt = "You have received an email from ".$name.".\n\n".$message;
+
+	mail($recipient, $subject, $txt, $headers);
+	header("Location: Index.html?mailsend");
+}
+?>
