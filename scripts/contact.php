74871b8ff2c79c7e4b85b7cf8b8ddc44f49c4f6b
Santiago Beltran <beltran.santiago1@gmail.com>
2019-01-17 19:45:02 -0800

    updated contack php
    Fixed some syntax

--- scripts/contact.php
+++ scripts/contact.php
@@ -1,12 +1,12 @@
 ﻿<?php
 if(isset($_POST["submit"])) {
 	$name= $_POST["name"];
-	$subject= $_POST["subject"]
-	$emailFrom= $_POST["mail"];
+    $emailFrom= $_POST["email"];
+	$subject= $_POST["subject"];
 	$message= $_POST["message"];
 
     $recipient= "Oruga.95@hotmail.com";
-	$headers ="From: ".$emailFrom;
+	$headers ="From: "$emailFrom;
 	$txt = "You have received an email from ".$name.".\n\n".$message;
 
 	mail($recipient, $subject, $txt, $headers);
