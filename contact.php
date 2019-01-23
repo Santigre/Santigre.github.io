c447d1252c61f54d9f90c64b5474080f80cedaf6
Santiago Beltran <beltran.santiago1@gmail.com>
2019-01-22 19:19:36 -0800

    Testing
    This version works on my end so far. Testing it live

--- contact.php
+++ contact.php
@@ -1,25 +1,6 @@
-﻿<?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
-$name = $_POST['name'];
-$visitor_email = $_POST['email'];
-$message = $_POST['message'];
+﻿
+<?php
 
-//Validate first
-if(empty($name)||empty($visitor_email)) 
-{
-    echo "Name and email are mandatory!";
-    exit;
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
 
 	$name= $_POST["name"];
     $emailFrom= $_POST["email"];
@@ -32,27 +13,5 @@ if(IsInjected($visitor_email))
 
 	mail($recipient, $subject, $txt, $headers);
 	header("Location: index.html?mailsend");
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
-    {
-    return true;
-  }
-  else
-    {
-    return false;
-  }
-}
    
 ?> 
